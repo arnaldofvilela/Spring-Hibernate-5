42e507b6b41dbdb2b3110a1d928562d57ea5dbcd
Arnaldo Neto <vilela-neto@hotmail.com>
2022-03-18 22:15:57 -0300

    31. Spring Dependency Injection - Write Some Code - Part 1

--- src/com/luv2code/springdemo/TestCoach.java
+++ src/com/luv2code/springdemo/TestCoach.java
@@ -5,4 +5,9 @@ public class TestCoach implements Coach{
     public String getDailyWorkout() {
         return "This is a test of interface implementation!";
     }
+
+    @Override
+    public String getDailyFortune() {
+        return "Test Message";
+    }
 }
